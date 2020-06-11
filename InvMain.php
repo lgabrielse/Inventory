f38295d763f920bc68e8683120e12c695a9c297e
Len Gabrielse <lgabrielse@verizon.net>
2020-06-10 20:35:34 -0400

    first three files

---
+++ InvMain.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div class="main">
+  <h2>Inventory Main </h2>
+</div>
+</body>
+</html>
---
+++ InvPage1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div align="center">
+<div style="background-color:Gray; width:599px; " align="center">
+  <h1>PAGE 1 </h1>
+</div>
+</div>
+</body>
+</html>
