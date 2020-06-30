090c9482a9e2de40b5ff08307b27cfad0c7c251b
Len Gabrielse <lgabrielse@verizon.net>
2020-06-30 13:33:59 -0400

    commit canges 2 files plus one delete

---
+++ InvPage3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>InvPage3</title>
+</head>
+
+<body>
+
+<h>InvPage3</h1>
+</body>
+</html>
