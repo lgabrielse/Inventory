aef5d4cc04525cc454210fecd23de5590bda9247
Len Gabrielse <lgabrielse@verizon.net>
2020-07-02 13:45:51 -0400

    created page 5

---
+++ InvPage5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>page5</title>
+</head>
+
+<body>
+<h1>&nbsp;page 5 </h1>
+<h1>&nbsp;page 5 update </h1>
+</body>
+</html>
