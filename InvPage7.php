fe0457122aa4374b2cc55f25bdd87a2c271b8b7a
Len Gabrielse <lgabrielse@verizon.net>
2020-07-07 11:52:40 -0400

    page2 and add 7

--- InvPage7.php
+++ InvPage7.php
@@ -2,13 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Inv-Pg2</title>
+<title>Inv-Pg7</title>
 </head>
 
 <body>
-<h1><strong>Page 2
-</strong></h1>
+<h1><strong>Page 7</strong></h1>
 
-<p>Added to page 2</p>
 </body>
 </html>
