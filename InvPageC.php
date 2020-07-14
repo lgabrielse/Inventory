32645f68f90306045a8468fceb9d598f07945cf2
Leonard Gabrielse <lgabrielse@verizon.net>
2020-07-14 15:47:16 -0400

    Create InvPageC.php
    Created InvPageC

---
+++ InvPageC.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PageC</title>
+</head>
+
+<body>
+<div align="center">
+<div style="background-color:Gray; width:599px; " align="center">
+  <h1>PAGE 1C </h1>
+  <h1>PAGE 2C edited </h1>
+  <h1>PAGE 3C edited  again</h1>
+</div>
+</div>
+</body>
+</html>
