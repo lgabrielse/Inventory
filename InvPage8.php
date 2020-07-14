510829aeca21580ab2b3216432af0bff2c3ca779
Leonard Gabrielse <lgabrielse@verizon.net>
2020-07-14 11:14:10 -0400

    add pg 8 and mod pg 7
    add pg 8 and mod pg 7
    pg 8 has php

--- InvPage8.php
+++ InvPage8.php
@@ -1,12 +1,15 @@
+<?php echo 'Page Eight'?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Inv-Pg7</title>
+<title>Inv-Pg8</title>
 </head>
 
 <body>
-<h1><strong>Page 7</strong></h1>
-<h1><strong>Page 7 added a change 7-14</strong></h1>
+<h1><strong>Page 8
+</strong></h1>
+
+<p></p>
 </body>
 </html>
