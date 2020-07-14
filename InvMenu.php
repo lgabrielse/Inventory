7dbb35b0d6ecddbd9804c11adac63e71033203ed
Leonard Gabrielse <lgabrielse@verizon.net>
2020-07-14 13:23:44 -0400

    Create InvMenu.php
    Created Menu page

---
+++ InvMenu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>InvMenu</title>
+</head>
+
+<body>
+<div class="menu">
+  <h2>1. Main</h2>
+  <h2>2. page 4</h2>
+  <h2>3. Page 9 </h2>
+</div>
+</body>
+</html>
