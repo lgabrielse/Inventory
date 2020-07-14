44e1510b0165e8fbc7e9bccc5c2eb813a1ed0284
Leonard Gabrielse <lgabrielse@verizon.net>
2020-07-14 13:40:46 -0400

    Added page A in MenuBranch

---
+++ InvPageA.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>pageA</title>
+</head>
+
+<body>
+<h1>&nbsp;page A </h1>
+<h1>&nbsp;page A update </h1>
+<h1>&nbsp;page A update 2</h1>
+</body>
+</html>
